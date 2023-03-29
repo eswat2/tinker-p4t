111757f0c6f993dddb2d55aae62ae27fcaaa599d
Richard Hess <eswat2@gmail.com>
2023-03-29 10:12:29 -0700

    » updating packages

--- src/components/Eswat2.jsx
+++ src/components/Eswat2.jsx
@@ -6,7 +6,7 @@ const who = "eswat2"
 const Eswat2 = () => {
   return (
     <a
-      class="text-clrs-gray hover:text-clrs-navy absolute top-8 right-8"
+      class="text-clrs-gray hover:text-clrs-navy absolute right-8 top-8"
       href={url}
       aria-label={who}
       target="blank"
